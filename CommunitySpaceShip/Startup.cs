a89aa120cdda8b73902efa4f7487114c0d6fbcbf
Luke-Beauchamp <luke.a.beauchamp@gmail.com>
2020-11-01 15:41:54 -0800

    Initial Commit - Established base UI Layer          -Continuing to: -Install Identity Samples -Establish Database Connection -Establish Initial Users & Roles

---
+++ CommunitySpaceShip/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CommunitySpaceShip.Startup))]
+namespace CommunitySpaceShip
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
